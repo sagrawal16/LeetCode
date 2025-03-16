82c6439dc17ed1f137ed11cfcc9669a9802b2e57
sagrawal16 <126394786+sagrawal16@users.noreply.github.com>
2025-03-16 23:13:04 +0530

    Time: 96 ms (14.52%), Space: 56.6 MB (24.19%) - LeetHub

---
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long repairCars(int[] ranks, int cars) {
+        long max = 0;
+        for(int num : ranks){
+            max = Math.max(max,num);
+        }
+        long right = max*cars*cars;
+        long left = 1;
+        while(left <right){
+            long mid = left +(right-left)/2;
+            if(canRepair(ranks,mid,cars)){
+                right = mid;
+            }else{
+                left = mid+1;
+            }
+        }
+        return left;
+    }
+    public boolean canRepair(int[] ranks,long mid,int cars){
+        int count = 0;
+        for(int i =0;i<ranks.length;i++){
+            count += Math.floor(Math.sqrt(mid/ranks[i]));
+        }
+        return count >= cars;
+    }
+}
