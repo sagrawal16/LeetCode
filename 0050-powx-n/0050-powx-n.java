943872e729a229bd22b9a10cc86c18997370419a
sagrawal16 <126394786+sagrawal16@users.noreply.github.com>
2025-08-09 15:54:09 +0530

    Time: 0 ms (100%), Space: 41.9 MB (88.62%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,23 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+        if(N < 0) {
+            return 1.0 / power(x, -N);
+        }
+        return power(x, N);
+    }
+
+    public double power(double x, long n) {
+        if(n == 0) {
+            return 1.0;
+        }
+        if(n == 1) {
+            return x;
+        }
+
+        if(n % 2 == 1) {
+            return x * power(x, n - 1);
+        } 
+        return power(x * x, n / 2);
+    }
+}
