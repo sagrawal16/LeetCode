fcc488b530810a3b50bf09248166564811e0a8d3
sagrawal16 <126394786+sagrawal16@users.noreply.github.com>
2026-02-03 17:53:09 +0530

    Time: 0 ms (100%), Space: 44.9 MB (10.08%) - LeetHub

---
+++ 3637-trionic-array-i/3637-trionic-array-i.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isTrionic(int[] nums) {
+        int n = nums.length;
+        if(n <= 3 || (nums[0] >= nums[1])) {
+            return false;
+        }
+        int count = 1;
+        for(int i = 2; i < n; i++) {
+            if(nums[i - 1] == nums[i]) {
+                return false;
+            }
+            if ((nums[i - 2] - nums[i - 1]) * (nums[i - 1] - nums[i]) < 0) {
+                count++;
+            }
+        }
+        return count == 3;
+    }
+}
